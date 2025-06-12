da8ce9355ab11099eaa927e35a09db1d76b4fc31
Victor Calbucci <victor@calbucci.com>
2025-06-12 14:39:59 -0700

    Added creat file util

--- src/main/java/util/FileUtils.java
+++ src/main/java/util/FileUtils.java
@@ -1,9 +1,20 @@
 package util;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.io.IOException;
+
 public class FileUtils {
-    
 
     public static void createFile(String path, byte[] content) {
-        
+        try {
+            Files.write(
+                    Paths.get(path),
+                    content,
+                    StandardOpenOption.CREATE_NEW);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to create file: " + path, e);
+        }
     }
 }
